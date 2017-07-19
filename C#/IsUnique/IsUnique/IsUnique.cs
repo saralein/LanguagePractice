324c54d1aab7ce5c91e22d0ff8291427eda1dd7d
saralein <sara.m.trujillo@gmail.com>
2017-07-19 17:31:58 -0500

    Complete IsUnique.  Investigate possible refactor.

---
+++ C#/IsUnique/IsUnique/IsUnique.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IsUniqueKata
+{
+    public class IsUnique
+    {
+        public bool HasUniqueCharacters(String phrase)
+        {
+            if (phrase == "")
+            {
+                return false;
+            }
+
+            var dictionary = new Dictionary<char,int> ();
+            foreach (char x in phrase)
+            {
+                if (dictionary.ContainsKey(x))
+                {
+                    dictionary[x]++;
+                }
+                else
+                {
+                    dictionary.Add(x, 1);
+                }
+            }
+
+            return dictionary.Values.ToList().All(n => n == 1);
+        }
+    }
+}
---
+++ C#/IsUnique/IsUniqueTest/Test.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using IsUniqueKata;
+
+namespace IsUniqueTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void ReturnsFalseForEmptyString()
+        {
+            var str = new IsUnique();
+            Assert.That(str.HasUniqueCharacters(""), Is.EqualTo(false));
+        }
+
+        [Test()]
+        public void ReturnsFalseForAllSameCharacter()
+        {
+            var str = new IsUnique();
+            Assert.That(str.HasUniqueCharacters("aaa"), Is.EqualTo(false));
+        }
+
+        [Test()]
+        public void ReturnsTrueForAllUniqueCharacters()
+        {
+            var str = new IsUnique();
+            Assert.That(str.HasUniqueCharacters("abcdefghijkl"), Is.EqualTo(true));
+        }
+
+        [Test()]
+        public void ReturnsFalseForMultipleSpaces()
+        {
+            var str = new IsUnique();
+            Assert.That(str.HasUniqueCharacters("a b c"), Is.EqualTo(false));
+        }
+    }
+}
