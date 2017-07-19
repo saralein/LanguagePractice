558ea9f82946144f4f86d9076385a482b38b6604
saralein <sara.m.trujillo@gmail.com>
2017-07-18 19:39:22 -0500

    Complete find odd cubes in C#.  Refactor needed for tests and class.

---
+++ C#/FindOddCubes/FindOddCubes/FindOddCubes.cs
@@ -0,0 +1,20 @@
+﻿namespace FindOddCubesKata
+{
+    public class FindOddCubes
+    {
+        public int OddCubeSum(int[] numbers)
+        {
+
+            var sum = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 != 0)
+                {
+                    sum += number * number * number;
+                }
+            }
+            return sum; 
+        }
+    }
+}
---
+++ C#/FindOddCubes/FindOddCubesTest/Test.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using FindOddCubesKata;
+
+namespace FindOddCubesTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void ItReturnsZeroForEmptyArray()
+        {
+            var x = new FindOddCubes();
+            var actual = x.OddCubeSum(new int[] { });
+            Assert.That(actual, Is.EqualTo(0));
+        }
+
+        [Test()]
+        public void ItReturnsZeroForAllEvenNumbers()
+        {
+            var x = new FindOddCubes();
+            var actual = x.OddCubeSum(new int[] { 2, 4, 6 });
+            Assert.That(actual, Is.EqualTo(0));
+        }
+
+        [Test()]
+        public void ItReturnsCubeForOddNumber()
+        {
+            var x = new FindOddCubes();
+            var actual = x.OddCubeSum((new int[] { 2, 3, 4 }));
+            Assert.That(actual, Is.EqualTo(27));
+        }
+
+        [Test()]
+        public void ItPassesTestsFromCodeWars()
+        {
+            var x = new FindOddCubes();
+
+            Assert.That(x.OddCubeSum(new int[] { -5, -5, 5, 5 }), Is.EqualTo(0));
+            Assert.That(x.OddCubeSum(new int[] { 1, 2, 3, 4 }), Is.EqualTo(28));
+            Assert.That(x.OddCubeSum(new int[] { -3, -2, 2, 3 }), Is.EqualTo(0));
+        }
+    }
+}
