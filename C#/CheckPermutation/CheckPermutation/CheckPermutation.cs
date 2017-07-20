9e57c281eb3e2b80daaeebe055bcb4a567eff9a2
saralein <sara.m.trujillo@gmail.com>
2017-07-20 14:49:51 -0500

    Complete check permutations.  Make extension method for dictionary add/update?

---
+++ C#/CheckPermutation/CheckPermutation/CheckPermutation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckPermutationKata
+{
+    public class CheckPermutation
+    {
+        private bool IsNullWhiteSpaceOrDiffLength(string str1, string str2)
+        {
+            return string.IsNullOrWhiteSpace(str1) ||
+                   string.IsNullOrWhiteSpace(str2) ||
+                   str1.Length != str2.Length;
+        }
+
+        public bool ArePermutations(string str1, string str2) {
+            if (IsNullWhiteSpaceOrDiffLength(str1, str2))
+            {
+                return false;
+            }
+
+            var dictionary = new Dictionary<char, int>();
+            foreach (char x in str1)
+            {
+                if (dictionary.ContainsKey(x))
+                {
+                    dictionary[x]++;
+                }
+                else
+                {
+                    dictionary[x] = 1;
+                }
+            }
+
+            foreach (char x in str2)
+            {
+                if (!dictionary.ContainsKey(x))
+                {
+                    return false;
+                }
+                else
+                {
+                    dictionary[x]--;
+                }
+            }
+
+            return dictionary.All(kvp => kvp.Value == 0);
+        }
+    }
+}
---
+++ C#/CheckPermutation/CheckPermutationTest/Test.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using CheckPermutationKata;
+
+namespace CheckPermutationTest
+{
+    [TestFixture]
+    public class Test
+    {
+        
+        private CheckPermutation _perms;
+
+	    [SetUp]
+	    public void InitCheckPermutation()
+	    {
+            _perms = new CheckPermutation();
+	    }
+
+        [Test]
+        public void ItReturnsFalseForEmptyString()
+        {
+            Assert.That(_perms.ArePermutations("", ""), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ItReturnsFalseForDiffLenghts()
+        {
+            Assert.That(_perms.ArePermutations("aa", "aaa"), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ItReturnsFalseForDifferentStrings()
+        {
+            Assert.That(_perms.ArePermutations("cat", "acts"), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ItReturnsTrueForPermutation() {
+            Assert.That(_perms.ArePermutations("cheese", "heecse"), Is.EqualTo(true));
+        }
+    }
+}
