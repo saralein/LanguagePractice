60aa8b9602655fa1498de7e84336878dc46995fb
saralein <sara.m.trujillo@gmail.com>
2017-07-20 17:44:49 -0500

    Complete URLify.

---
+++ C#/URLify/URLify/URLify.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace URLifyKata
+{
+    public class URLify
+    {
+        public string ConvertStringToURL(string str, int trueLen)
+        {
+            var strArray = str.Substring(0, trueLen).Split(' ');
+            return string.Join("%20", strArray);
+        }
+    }
+}
---
+++ C#/URLify/URLifyTest/Test.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using URLifyKata;
+
+namespace URLifyTest
+{
+    [TestFixture]
+    public class Test
+    {
+        private URLify _str;
+
+        [SetUp]
+        public void InitURLify()
+        {
+            _str = new URLify();
+        }
+
+        [Test]
+        public void ReturnsEmptyStringForNoTrueLength ()
+        {
+            Assert.That(_str.ConvertStringToURL(" ", 0), Is.EqualTo(""));
+        }
+
+        [Test]
+        public void ReplacesOneSpaceWithChars ()
+        {
+            Assert.That(_str.ConvertStringToURL(" ", 1), Is.EqualTo("%20"));
+        }
+
+        [Test]
+        public void ReplacesSpaceInStringWithTrail ()
+        {
+            Assert.That(_str.ConvertStringToURL("Mr John Smith    ", 13), Is.EqualTo("Mr%20John%20Smith"));
+        }
+
+        [Test]
+        public void RelacesSpaceInStringOfTrueLength ()
+        {
+            Assert.That(_str.ConvertStringToURL("Mr John Smith", 13), Is.EqualTo("Mr%20John%20Smith"));
+        }
+    }
+}
